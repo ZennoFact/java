405d3776b3ebabfcee389cfe2c2e7f9f0181af32
Kazuki Maeno <zenno04.g@gmail.com>
2014-10-08 12:19:56 +0900

    Java#2追加

--- Java1_returns/src/VendingMachineReturns.java
+++ Java1_returns/src/VendingMachineReturns.java
@@ -5,40 +5,40 @@ public class VendingMachineReturns {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		String[] shohin={"冷やし味噌汁120円","沸騰コーラ120円","ナタデココ100%150円","怪物200円"};
-		int[] yen={120,120,150,200};
-		int[] count={0,0,0,0};
+		String[] s={"冷やし味噌汁120円","沸騰コーラ120円","ナタデココ100%150円","怪物200円"};
+		int[] y={120,120,150,200};
+		int[] c={0,0,0,0};
 
 
 		System.out.println("いらっしゃいませ"+"\n"+"お金を入れてください>");
-		int maney=sc.nextInt();
-		int num=0;
+		int m=sc.nextInt();
+		int n=0;
 
 		do{
-		if(maney>=120)
+		if(m>=120)
 		{
-		System.out.println("\n"+"残金は"+maney+"円です。"+"\n"+"\n購入番号を入力してください。");
-		System.out.println(shohin[0]+"\n"+shohin[1]+"\n"+shohin[2]+"\n"+shohin[3]);
+		System.out.println("\n"+"残金は"+m+"円です。"+"\n"+"\n購入番号を入力してください。");
+		System.out.println(s[0]+"\n"+s[1]+"\n"+s[2]+"\n"+s[3]);
 		System.out.print("購入番号(1～4)を選択してください。>");
-		num=sc.nextInt();
+		n=sc.nextInt();
 
-		if(num<1||num>4)
+		if(n<1||n>4)
 		{
 			break;
 		}
-		if(maney>=yen[num-1]){
-			maney=maney-yen[num-1];
-			System.out.println("\n"+shohin[num-1]+"が購入できました");
-			count[num-1]+=1;
+		if(m>=y[n-1]){
+			m=m-y[n-1];
+			System.out.println("\n"+s[n-1]+"が購入できました");
+			c[n-1]+=1;
 		   }else{
 				System.out.println("お金が足りません");
 		   		}
 		       }
 
 				}
-				while(maney>=120&&1<=num&&num<=4);
+				while(m>=120&&1<=n&&n<=4);
 
-			if((count[0]==0&&count[1]==0&&count[2]==0&&count[3]==0)||maney<120){
+			if((c[0]==0&&c[1]==0&&c[2]==0&&c[3]==0)||m<120){
 				System.out.println("");
 				System.out.println("購入金額が不足しています。"+"\n"+"またのご利用お待ちしてます");
 		}else{
@@ -46,10 +46,10 @@ public class VendingMachineReturns {
 			}
 
 		System.out.println("\n"+"～～購入した商品～～");
-		for (int i=0;i<count.length;i++){
-			if(count[i]>0)
-				System.out.println(shohin[i]+":"+count[i]+"本"+"です");
+		for (int i=0;i<c.length;i++){
+			if(c[i]>0)
+				System.out.println(s[i]+":"+c[i]+"本"+"です");
 		}
-		System.out.println("残金は"+maney+"円です。");
+		System.out.println("残金は"+m+"円です。");
 	}
 }
